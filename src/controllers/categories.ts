ec660eacfb2fd80aa3e60ec39b37577b199b5588
Ebun22 <subeydavid22@gmail.com>
2024-11-29 23:33:28 +0100

    bug introduced

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,5 +1,14 @@
-export const createCategories = async () => {
+import { NextFunction, Request, Response } from "express"
+import { CategorySchema } from "../schema/category"
 
+export const createCategories = async (req: Request, res: Response, nexxt: NextFunction) => {
+    CategorySchema.parse(req.body);
+    const category = await category.createMany({
+        data: {
+            name: cat.name,
+            parentId: cat.parentId,
+        }
+    })
 }
 
 export const getCategories = async () => {
