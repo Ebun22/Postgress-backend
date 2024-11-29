87d3d98f59d5f7f51da9039459df6f51780d047f
Ebun22 <subeydavid22@gmail.com>
2024-11-29 23:28:00 +0100

    created category controller

---
+++ src/controllers/categories.ts
@@ -0,0 +1,15 @@
+export const createCategories = async () => {
+
+}
+
+export const getCategories = async () => {
+
+}
+
+export const getCategoriesById = async () => {
+
+}
+
+export const deleteCategories = async () => {
+
+}
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { errorHandler } from "../error-handler";
 import { authMiddleWare } from "../middlewares/auth";
 import { cancelOrders, createOrder, getOrderById, listOrders } from "../controllers/orders";
+import { adminMiddleWare } from "../middlewares/admin";
 
 const catgoriesRoutes: Router = Router()
 
 
-catgoriesRoutes.post("/", [authMiddleWare], errorHandler(createOrder))
+catgoriesRoutes.post("/", [authMiddleWare, adminMiddleWare], errorHandler(createOrder))
 catgoriesRoutes.get("/", [authMiddleWare], errorHandler(listOrders))
 catgoriesRoutes.get("/:id", [authMiddleWare], errorHandler(getOrderById))
-catgoriesRoutes.put("/:id", [authMiddleWare], errorHandler(cancelOrders))
+catgoriesRoutes.put("/:id", [authMiddleWare, adminMiddleWare], errorHandler(cancelOrders))
 
 export default catgoriesRoutes;
