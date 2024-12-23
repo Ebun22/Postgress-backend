81cbb40670d41e23bc0d200fe30d9847245f66b7
Ebun22 <subeydavid22@gmail.com>
2024-12-23 11:48:47 +0100

    added get all customers route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { errorHandler } from "../error-handler";
 import { authMiddleWare } from "../middlewares/auth";
 import { adminMiddleWare } from "../middlewares/admin";
-import { createShippingAddress, deleteShippingAddress, editShippingAddress, editUser, getByIdShippingAddress, getShippingAddress, getUser} from "../controllers/users";
+import { createShippingAddress, deleteShippingAddress, editShippingAddress, editUser, getAllUser, getByIdShippingAddress, getShippingAddress, getUser} from "../controllers/users";
 
 const usersRoutes: Router = Router()
 
@@ -10,6 +10,7 @@ const usersRoutes: Router = Router()
 usersRoutes.put("/", [authMiddleWare], errorHandler(editUser))
 usersRoutes.get("/", [authMiddleWare], errorHandler(getUser))
 
+usersRoutes.get("/", [authMiddleWare], errorHandler(getAllUser))
 usersRoutes.get("/address", errorHandler(getShippingAddress))
 usersRoutes.get("/address/:id", errorHandler(getByIdShippingAddress))
 usersRoutes.post("/address", [authMiddleWare], errorHandler(createShippingAddress))
