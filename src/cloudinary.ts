3491a5b4df279e02a10687f93b90a3b2d0330a5a
Ebun22 <subeydavid22@gmail.com>
2025-01-01 23:54:12 +0100

    trying fix one

--- src/cloudinary.ts
+++ src/cloudinary.ts
@@ -4,7 +4,7 @@ import { API_KEY, API_SECRET, CLOUD_NAME } from './secrets';
 cloudinary.config({ 
     cloud_name: CLOUD_NAME, 
     api_key: '5382632810901', 
-    api_secret: API_SECRET // Click 'View API Keys' above to copy your API secret
+    api_secret: 'fty53y838diwaeo3092yd8wuhu' // Click 'View API Keys' above to copy your API secret
 });
 
 export default cloudinary
