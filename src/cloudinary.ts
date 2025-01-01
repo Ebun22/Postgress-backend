6120aa53ad13f268a8fe26469509cfb595054b05
Ebun22 <subeydavid22@gmail.com>
2025-01-01 23:33:12 +0100

    trying fix one

--- src/cloudinary.ts
+++ src/cloudinary.ts
@@ -3,7 +3,7 @@ import { API_KEY, API_SECRET, CLOUD_NAME } from './secrets';
 
 cloudinary.config({ 
     cloud_name: CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
+    api_key: '5382632810901', 
     api_secret: API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
