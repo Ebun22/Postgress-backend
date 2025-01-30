beada0505f93f792a9984fd98835bbd9f3eb5d30
Ebun22 <subeydavid22@gmail.com>
2025-01-30 12:45:10 +0100

    created search on orders

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
 import { authMiddleWare } from "../middlewares/auth";
-import { createCheckout, createOrder, deleteOrder, EditOrderStatus, getOrderById, listOrders, totalOnOrderDashboard } from "../controllers/orders";
+import { createCheckout, createOrder, deleteOrder, EditOrderStatus, getOrderById, getOrdersBySearch, listOrders, totalOnOrderDashboard } from "../controllers/orders";
 import { adminMiddleWare } from "../middlewares/admin";
 
 const orderRoutes: Router = Router()
@@ -13,7 +13,7 @@ orderRoutes.post("/", [authMiddleWare], errorHandler(createOrder))
 orderRoutes.post("/:orderId/checkout", [authMiddleWare], errorHandler(createCheckout))
 orderRoutes.post("/:orderId/stripe/webhook", [authMiddleWare], errorHandler(createCheckout))
 orderRoutes.get("/:id", [authMiddleWare], errorHandler(getOrderById))
-orderRoutes.get("/search/:search", [authMiddleWare], errorHandler(getOrderById))
+orderRoutes.get("/search/:search", [authMiddleWare], errorHandler(getOrdersBySearch))
 orderRoutes.put("/:id", [authMiddleWare], errorHandler(EditOrderStatus))
 orderRoutes.delete("/:id",[authMiddleWare, adminMiddleWare], errorHandler(deleteOrder))
 
