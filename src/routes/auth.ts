456a4c8caf52cf3782656d85605d93be7aaf6d5d
Ebun22 <subeydavid22@gmail.com>
2025-02-04 23:24:50 +0100

    fixed otp login bot

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,8 +8,8 @@ const authRoutes:Router = Router();
 authRoutes.post('/signup', errorHandler(signup));
 authRoutes.post('/login', errorHandler(login));
 authRoutes.post('/forgotpassword', errorHandler(forgotPassword));
-authRoutes.post('/getOTP', errorHandler(getOTP));
-authRoutes.post('/OTPLogin', errorHandler(OTPLogin));
+authRoutes.post('/login/getotp', errorHandler(getOTP));
+authRoutes.post('/login/otplogin', errorHandler(OTPLogin));
 authRoutes.patch('/resetPassword/:token', errorHandler(forgotPassword));
 
 export default authRoutes
